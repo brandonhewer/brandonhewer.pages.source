d999d1759320c6897f4e250c4705f0a29d626573
brandonhewer <bh532@bath.ac.uk>
2019-10-17 13:35:37 +0100

    Update travis config

--- script/make4htinstall.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -e # halt script on error
-
-apt-get install -y pandoc
-cd make4ht
-make
-make install
--- script/runmake4ht.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-set -e # halt script on error
-
-find _posts-tex/ -name "*.tex" -type f -exec make4ht -um publish {} "svg" \;
