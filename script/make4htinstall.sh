82070410659016c4775328ffb16fb154ad72ca80
brandonhewer <bh532@bath.ac.uk>
2019-10-17 13:23:18 +0100

    Update travis config

--- script/make4htinstall.sh
+++ script/make4htinstall.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -e # halt script on error
 
-apt-get install latexmk
-apt-get install pandoc
+apt-get install -y latexmk
+apt-get install -y pandoc
 cd make4ht
 make
 make install
