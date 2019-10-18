eb138de78cf34b718178c17008f0014e8a8ea0a4
brandonhewer <bh532@bath.ac.uk>
2019-10-18 22:19:53 +0100

    Update travis config

--- script/texbuild.sh
+++ script/texbuild.sh
@@ -5,6 +5,6 @@ tlmgr update --self
 tlmgr install luaxml
 tlmgr install tex4ht
 tlmgr install make4ht
+tlmgr install tidy
 tlmgr update --all
-sudo apt-get install tidy
 find ./_posts-tex -name "*.tex" -type f -exec make4ht -l -um publish {} "svg" \;
