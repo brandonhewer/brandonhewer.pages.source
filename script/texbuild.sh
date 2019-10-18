f55b160df0e8809fd31a2277d115f4195721012a
brandonhewer <bh532@bath.ac.uk>
2019-10-18 22:37:23 +0100

    Add tidy install

--- script/texbuild.sh
+++ script/texbuild.sh
@@ -6,5 +6,7 @@ tlmgr install luaxml
 tlmgr install tex4ht
 tlmgr install make4ht
 tlmgr update --all
-apt-get install tidy
+wget https://github.com/htacg/tidy-html5/releases/download/5.4.0/tidy-5.4.0-64bit.deb
+dpkg -i tidy-5.4.0-64bit.deb
+rm tidy-5.4.0-64bit.deb
 find ./_posts-tex -name "*.tex" -type f -exec make4ht -l -um publish {} "svg" \;
