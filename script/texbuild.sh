49c1cb43da717f0db3215383949171a6b16b5db4
brandonhewer <bh532@bath.ac.uk>
2019-10-18 22:45:38 +0100

    Update travis config

--- script/texbuild.sh
+++ script/texbuild.sh
@@ -9,4 +9,5 @@ tlmgr update --all
 wget https://github.com/htacg/tidy-html5/releases/download/5.4.0/tidy-5.4.0-64bit.deb
 dpkg -i tidy-5.4.0-64bit.deb
 rm tidy-5.4.0-64bit.deb
+rm -v .wgets-hsts
 find ./_posts-tex -name "*.tex" -type f -exec make4ht -l -um publish {} "svg" \;
