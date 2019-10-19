67030ea1f4bcc56dbd0ab99653ecfca1fc21742f
brandonhewer <bh532@bath.ac.uk>
2019-10-19 14:33:51 +0100

    Test tikz

--- script/texbuild.sh
+++ script/texbuild.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
 
 set -e
-tlmgr update --self
-tlmgr install luaxml
-tlmgr install tex4ht
-tlmgr install make4ht
-tlmgr update --all
 wget https://github.com/htacg/tidy-html5/releases/download/5.4.0/tidy-5.4.0-64bit.deb
 dpkg -i tidy-5.4.0-64bit.deb
 rm tidy-5.4.0-64bit.deb
