437b69e2931af0dc1fb3e71911d41118d2d493b7
brandonhewer <bh532@bath.ac.uk>
2019-10-19 22:52:45 +0100

    Ensure images are found in correct directory

--- script/texbuild.sh
+++ script/texbuild.sh
@@ -5,4 +5,4 @@ wget https://github.com/htacg/tidy-html5/releases/download/5.4.0/tidy-5.4.0-64bi
 dpkg -i tidy-5.4.0-64bit.deb
 rm tidy-5.4.0-64bit.deb
 rm -v .wget-hsts
-find ./_posts-tex -name "*.tex" -type f -exec make4ht -l -um publish {} "svg" \;
+find ./_posts-tex -name "*.tex" -type f -exec make4ht -l -um publish {} "svg,imgdir:/assets/images/" \;
